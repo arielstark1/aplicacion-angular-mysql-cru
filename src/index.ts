282382a56b79da196f12ba87b619f6c24709f64c
ariel st <arielmarvin@gmail.com>
2022-09-08 14:15:41 -0300

    cambio

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express, { Application } from "express";
+import indexRoutes from "./routes/indexRoutes";
+import gamesRoutes from "./routes/gameRoutes";
+
+class Server {
+  public app: Application;
+
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routes();
+  }
+  config(): void {
+    this.app.set("port", process.env.PORT || 3000);
+    this.app.use(express.json)
+    this.app.use(express.urlencoded({extended:false}))
+    
+  }
+  routes(): void {
+    this.app.use('/',indexRoutes);
+    this.app.use('/api/games',gamesRoutes);
+  }
+  start(): void {
+    this.app.listen(this.app.get("port"), () => {
+      console.log("Server on port ", this.app.get("port"));
+    });
+  }
+}
+const server = new Server();
+server.start();
